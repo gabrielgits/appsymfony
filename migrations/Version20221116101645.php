d1c33195d54f09d31956c526987bccf5a30ca987
Gabriel Vieira <gvgabrielvieiragabrielvieira@gmail.com>
2022-11-18 11:56:35 +0100

    home page

--- migrations/Version20221116101645.php
+++ migrations/Version20221116101645.php
@@ -23,6 +23,11 @@ final class Version20221116101645 extends AbstractMigration
         $this->addSql('CREATE TABLE news (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(128) NOT NULL, description VARCHAR(255) NOT NULL, picture LONGTEXT DEFAULT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    
+        if($schema->hasTable('user'))
+        {
+            $this->addSql("INSERT INTO `user` VALUES (null,'user@example.com','[]','$2y$13$iMJaQdji5l3EYW4vcBE.T.Vf1LHNNbD8YToPm1eiLvCMlu2vVCEwm'),(null,'admin@example.com','[\"ROLE_ADMIN\"]','$2y$13$xyik3FNb0YqGnWo5bMS5teqgV8DX4ny0uWEIAXT0iz/j8aE0JgP6q');");
+        }
     }
 
     public function down(Schema $schema): void
